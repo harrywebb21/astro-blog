0e9672be55199e581524f48687efccc217949964
harrywebb21 <harrywebb21@gmail.com>
2023-03-20 04:01:09 +0000

    testing notification

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
-    site: "https://my-blog-site.netlify.app",
+    site: "https://main--lambent-macaron-b025fe.netlify.app/",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-us</language>`,
   });
